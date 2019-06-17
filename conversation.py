ec8fb317391f29238a70fc17ecb7dc50720c7f1c
shira weitman <shiraweitman@gmail.com>
2019-06-17 18:11:55 +0300

    Merge branch 'master' of https://github.com/shiraweitman/movieAnalyze into shira
    # Conflicts:
    #	CreateData.py
    #	Scene.py

--- conversation.py
+++ conversation.py
@@ -1,9 +1,13 @@
 class Conversation:
 
-    def __init__(self, movie, character_set, lines):
+    lines_dict = {}
+    LINES_SEPERATOR = ", "
+
+    def __init__(self, movie, character_set, lines, text):
         self.movie = movie
         self.character_set = character_set
         self.male_characters = set()
+        self.text = text
         self.lines = lines # one string with all lines
         for character in self.character_set:
             if character.get_gender() == "m":
@@ -21,3 +25,14 @@ class Conversation:
     def set_character(self, old_val, new_val):
         self.character_set.remove(old_val)
         self.character_set.add(new_val)
+
+    def get_lines_text(self):
+        conversation_text = ""
+        lines = self.lines.split(self.LINES_SEPERATOR)
+        for line in lines:
+            line1 = line.strip("[")
+            line2 = line1.strip("]")
+            line3 = line2.strip("'")
+            text = self.lines_dict.get(line3)
+            conversation_text = conversation_text+text
+        self.text = conversation_text
