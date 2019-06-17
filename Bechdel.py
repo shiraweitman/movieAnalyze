1e1b6bc527013d2d17c65ff82d9b9526afd68d56
michal <michalgordon92@gmail.com>
2019-06-17 19:48:53 +0300

    bechdel men

--- Bechdel.py
+++ Bechdel.py
@@ -7,6 +7,7 @@ import Conversation
 class Bechdel:
 
     male_references = ["him", "his", "he"]
+    female_references = ["her", "she", "her's"]
     NOT_PASSED = False
     PASSED = True
 
@@ -28,22 +29,19 @@ class Bechdel:
 
     def run_bechdel_test(self):
         self.first()
+        self.bechdel_men1()
         self.second()
         print(self.__movie.movie_name)
         print(str(self.test1) + " bechdel test 1")
         print(str(self.test2) + " bechdel test 2")
         print(str(self.test3) + " bechdel test 3")
-        over_all = 0
-        if(self.test1 == Bechdel.PASSED):
-            over_all+=1
-        if(self.test2 == Bechdel.PASSED):
-            over_all+=1
-        if(self.test2 == Bechdel.PASSED):
-            over_all+=1
-        if over_all ==3:
-            self.overall = Bechdel.PASSED
+        if(self.test1&self.test2&self.test3):
+            self.overall = True
             print(self.__movie.movie_name+ " passed bechdel test")
         else: print(self.__movie.movie_name+ " did not pass bechdel test")
+        if (self.men1 & self.men2 & self.men3):
+            self.overall_men = True
+            print(self.__movie.movie_name+ " passed men bechdel test")
 
 
 
@@ -67,6 +65,8 @@ class Bechdel:
                     self.test3 = Bechdel.PASSED
             if self.get_gender_in_conversation(conversation, "m"):
                 self.men2 = True
+                if(self.bechdel_men3(conversation)):
+                    self.men3 = True
 
 
 
@@ -92,64 +92,65 @@ class Bechdel:
                 return True
         return False
 
-    def bechdel_men(self):
+    def bechdel_men1(self):
         if(len(self.__movie.get_male())>=2):
             self.men1 = True
 
-    def test_men3(self, conversation):
-        female_characters = conversation.get_male_characters()
-        for word in conversation.txt.split():
-            if word in Bechdel.male_references or word in male_characters:
+    def bechdel_men3(self, conversation):
+        female_characters = conversation.get_female_characters()
+        for word in conversation.text.split():
+            if word in Bechdel.female_references or word in female_characters:
                 return False
         return True
 
 
 
 
-# character1 = Character.Character(1,"CAMERON", "10 things", "m")
-# character2 = Character.Character(2,"MICHAEL", "10 things", "m")
-# characters_1 = set()
-# characters_1.add(character1)
-# characters_1.add(character2)
-# text = "That girl -- I -- You burn, you pine, you perish? Who is she? Bianca Stratford.  Sophomore. Don't even think about it "
-# con1 = conversation.Conversation("7",characters_1, text)
-#
-# character3 = Character.Character(3,"BIANCA", "10 things", "f")
-# character4 = Character.Character(4,"KAT", "10 things", "f")
-# characters_2 = set()
-# characters_2.add(character3)
-# characters_2.add(character4)
-#
-# text2 = "Can you turn down the Screaming Menstrual Bitches?  I'm trying to " \
-#         "becoming normal? It means that Gigglepuss is playing at Club Skunk and" \
-#         " we're going. Oh, I thought you might have a date.I don't know why" \
-#         " I'm bothering to ask, but are you going to Bogey Lowenstein's " \
-#         "party Saturday night? What do you think? I think you're'a ' \
-#         'freak.  I think you do this to torture me.  And I think you suck. "
-# con2 = conversation.Conversation("7",characters_2, text2)
-# movie_characters = set()
-# movie_characters.add(character1)
-# movie_characters.add(character2)
-# movie_characters.add(character3)
-# movie_characters.add(character4)
+character1 = Character.Character(1,"CAMERON", "10 things", "m")
+character2 = Character.Character(2,"MICHAEL", "10 things", "m")
+characters_1 = set()
+characters_1.add(character1)
+characters_1.add(character2)
+text = "That girl -- I -- You burn, you pine, you perish? Who is she? Bianca Stratford.  Sophomore. Don't even think about it "
+con1 = Conversation.Conversation("7",characters_1,[], text)
 
-# text2 = "Can you turn down the Screaming Menstrual Bitches?  I'm trying to " \
-#         "becoming normal? It means that Gigglepuss is playing at Club Skunk and" \
-#         " we're going. Oh, I thought you might have a date.I don't know why" \
-#         " I'm bothering to ask, but are you going to Bogey Lowenstein's " \
-#         "party Saturday night? What do you think? I think you're'a ' \
-#         'freak.  I think you do this to torture me.  And I think you suck. "
-# con2 = conversation.Conversation("7",characters_2, text2)
-# movie_characters = set()
-# movie_characters.add(character1)
-# movie_characters.add(character2)
-# movie_characters.add(character3)
-# movie_characters.add(character4)
+character3 = Character.Character(3,"BIANCA", "10 things", "f")
+character4 = Character.Character(4,"KAT", "10 things", "f")
+characters_2 = set()
+characters_2.add(character3)
+characters_2.add(character4)
 
-# movie = Movie.Movie(1, "try movie", [con1, con2], movie_characters, "comedy")
+text2 = "Can you turn down the Screaming Menstrual Bitches?  I'm trying to " \
+        "becoming normal? It means that Gigglepuss is playing at Club Skunk and" \
+        " we're going. Oh, I thought you might have a date.I don't know why" \
+        " I'm bothering to ask, but are you going to Bogey Lowenstein's " \
+        "party Saturday night? What do you think? I think you're'a ' \
+        'freak.  I think you do this to torture me.  And I think you suck. "
+con2 = Conversation.Conversation("7",characters_2,[], text2)
+movie_characters = set()
+movie_characters.add(character1)
+movie_characters.add(character2)
+movie_characters.add(character3)
+movie_characters.add(character4)
 #
-# test = Bechdel(movie)
-# test.run_bechdel_test()
+text2 = "Can you turn down the Screaming Menstrual Bitches?  I'm trying to " \
+        "becoming normal? It means that Gigglepuss is playing at Club Skunk and" \
+        " we're going. Oh, I thought you might have a date.I don't know why" \
+        " I'm bothering to ask, but are you going to Bogey Lowenstein's " \
+        "party Saturday night? What do you think? I think you're'a ' \
+        'freak.  I think you do this to torture me.  And I think you suck. "
+con2 = Conversation.Conversation("7",characters_2,[], text2)
+movie_characters = set()
+movie_characters.add(character1)
+movie_characters.add(character2)
+movie_characters.add(character3)
+movie_characters.add(character4)
+#
+movie = Movie.Movie(1, "try movie", [con1, con2], movie_characters,
+                    "comedy", 1991)
+movie.set_character_genders()
+test = Bechdel(movie)
+test.run_bechdel_test()
 
 
 
--- conversation.py
+++ conversation.py
@@ -1,11 +1,16 @@
 class Conversation:
 
-    def __init__(self, movie, character_set, lines, txt):
+    lines_dict = {}
+    LINES_SEPERATOR = ", "
+
+    def __init__(self, movie, character_set, lines, text):
         self.movie = movie
         self.character_set = character_set
         self.male_characters = set()
-        self.lines = lines
-        self.txt = txt
+        self.female_characters = set()
+
+        self.text = text
+        self.lines = lines # one string with all lines
 
 
     def get_characters(self):
@@ -17,11 +22,24 @@ class Conversation:
     def get_female_characters(self):
         return self.female_characters
 
-
     def get_lines(self):
         return self.lines
 
     def set_character(self, old_val, new_val):
         self.character_set.remove(old_val)
         self.character_set.add(new_val)
+        if new_val.gender == "m":
+            self.male_characters.add(new_val)
+        elif new_val.gender == "f":
+            self.female_characters.add(new_val)
 
+    def get_lines_text(self):
+        conversation_text = ""
+        lines = self.lines.split(self.LINES_SEPERATOR)
+        for line in lines:
+            line1 = line.strip("[")
+            line2 = line1.strip("]")
+            line3 = line2.strip("'")
+            text = self.lines_dict.get(line3)
+            conversation_text = conversation_text+text
+        self.text = conversation_text
